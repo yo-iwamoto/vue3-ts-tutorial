ab4e4bc216e4370feebdac863cf300606c805655
you-5805 <you.iwamoto918@gmail.com>
2021-03-26 04:04:54 +0900

    change host

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import baseAxios from 'axios'
 
 const axios = baseAxios.create({
-  // baseURL: 'https://leisurely-todo.herokuapp.com/api/v1'
-  baseURL: 'http://localhost:5000/api/v1'
+  baseURL: 'https://leisurely-todo.herokuapp.com/api/v1'
+  // baseURL: 'http://localhost:5000/api/v1'
 })
 
 export default axios
