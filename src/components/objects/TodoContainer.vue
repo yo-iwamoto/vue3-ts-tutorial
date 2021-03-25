58ce8ab5be1048db98fead2f806d370f914495e9
you-5805 <you.iwamoto918@gmail.com>
2021-03-26 02:04:58 +0900

    transition and layout of todo

---
+++ src/components/objects/TodoContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+})
+</script>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -3,7 +3,11 @@
   <div class="flex items-center">
     <div class="container mx-auto">
       <div class="mx-auto my-10 p-5 rounded-md">
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="fade" mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </router-view>
       </div>
     </div>
   </div>
@@ -19,3 +23,15 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
