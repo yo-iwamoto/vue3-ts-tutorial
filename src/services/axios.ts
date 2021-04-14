3903f8b6efd43ddb08f0626ed200fddc318a9ed3
you-5805 <you.iwamoto918@gmail.com>
2021-04-14 12:32:35 +0900

    remove CORS headers

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,11 +1,11 @@
 import baseAxios from 'axios'
 
 const axios = baseAxios.create({
-  baseURL: 'https://yo-express-todo-back.herokuapp.com/api/v1',
-  // baseURL: 'http://localhost:5000/api/v1',
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
+  baseURL: 'https://yo-express-todo-back.herokuapp.com/api/v1'
+  // baseURL: 'http://localhost:5000/api/v1'
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*'
+  // }
 })
 
 export default axios
